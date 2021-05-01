eb57b2617472a459d2bf5bc748c985fc24dc42f8
anderson-santiago-silva <uusantiago@gmail.com>
2021-04-30 22:45:14 -0300

    Propris update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
           <img src="/images/menu-top.svg" alt="ironhack-logo"/>
         </div>
 
-        <div class="main-title">
+        <div className="main-title">
           <h1>Say hello to ReactJS</h1>
           <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
 
@@ -22,22 +22,30 @@ const App = () => {
       </section>
 
       <section className="box-container">
-        <Card/>
-        <div>
-          <img src="/images/icon2.png" alt="icon2"/>
-          <h3>Components</h3>
-          <p>Build encapsulated components that manage their state.</p>
-        </div>
-        <div>
-          <img src="/images/icon3.png" alt="icon3"/>
-          <h3>Single-Way</h3>
-          <p>A set of immutable values are passed to the component's.</p>
-        </div>
-        <div>
-          <img src="/images/icon4.png" alt="icon4"/>
-          <h3>JSX</h3>
-          <p>Statically-typed, designed to run on modern browsers.</p>
-        </div>
+        <Card
+          imagePath="/images/icon1.png"
+          imageName="icon1"
+          title="Declarative"
+          description="React makes it painless to create interactive UIs."
+        />
+        <Card
+          imagePath="/images/icon2.png"
+          imageName="icon2"
+          title="Components"
+          description="Build encapsulated components that menage their state."
+        />
+        <Card
+          imagePath="/images/icon3.png"
+          imageName="icon3"
+          title="Single-Way"
+          description="A set of immutable values are passed to the component's."
+        />
+        <Card
+          imagePath="/images/icon4.png"
+          imageName="icon4"
+          title="JSX"
+          description="Statically-typed, designed to run on modern browsers."
+        />
       </section>
 
     </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './Card.css';
 
-const Card = () => {
-	return (
-		<div>
-			<img src="/images/icon1.png" alt="icon1"/>
-			<h3>Declarative</h3>
-			<p>React makes it painless to create interactive UIs.</p>
-		</div>
-	);
+const Card = ({imagePath, imageName, title, description}) => {
+  return (
+    <div className='container-card'>
+      <img src={imagePath} alt={imageName}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default Card;
